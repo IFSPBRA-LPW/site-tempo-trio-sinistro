1f80a272223d44f316478d30efa0349555513ae9
Chico <souza.francisco@aluno.ifsp.edu.br>
2026-03-30 23:30:36 -0300

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,28 +1,57 @@
-const cityWeather = {
-    city: "Berlin",
-    country: "Germany",
-    temperature: 20,
-    feelsLike: 18,
-    humidity: 46,
-    wind: 14,
-    precipitation: 0,
-    daily: [
-        { day: "Tue", icon: "🌧", max: 20, min: 14 },
-        { day: "Wed", icon: "🌧", max: 21, min: 15 },
-        { day: "Thu", icon: "☀", max: 24, min: 14 },
-        { day: "Fri", icon: "🌧", max: 25, min: 13 },
-        { day: "Sat", icon: "⛈", max: 21, min: 15 },
-        { day: "Sun", icon: "☁", max: 25, min: 16 },
-        { day: "Mon", icon: "🌫", max: 24, min: 15 },
-    ],
-    hourly: [
-        { time: "3 PM", temp: 20 },
-        { time: "4 PM", temp: 20 },
-        { time: "5 PM", temp: 20 },
-        { time: "6 PM", temp: 19 },
-        { time: "7 PM", temp: 18 },
-        { time: "8 PM", temp: 18 },
-        { time: "9 PM", temp: 17 },
-        { time: "10 PM", temp: 17 },
-    ],
-};
+import { API_KEY } from "./config.js"
+
+
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const busca = document.querySelector("#busca").value
+    fetchWeather(busca)
+})
+
+
+
+export async function fetchWeather(city) {
+    const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&lang=pt`,
+    )
+    if (!response.ok) {
+        throw new Error("Erro ao buscar dados da API")
+    }
+    return response.json()
+}
+
+export async function getCityWeather(city) {
+    const data = await fetchWeather(city)
+    return adaptWeatherData(data)
+}
+
+export function adaptWeatherData(data) {
+    return {
+        city: data.location.name,
+        country: data.location.country,
+
+        date: data.location.localtime,
+
+        icon: data.current.condition.icon,
+        temperature: data.current.temp_c,
+        feelsLike: data.current.feelslike_c,
+        humidity: data.current.humidity,
+        wind: data.current.wind_kph,
+        precipitation: data.current.precip_mm,
+
+        daily: data.forecast.forecastday.map(day => ({
+            day: new Date(day.date).toLocaleDateString("pt-BR", { weekday: "short" }),
+            icon: day.day.condition.icon,
+            max: day.day.maxtemp_c,
+            min: day.day.mintemp_c
+        })),
+
+        hourly: data.forecast.forecastday[0].hour.map(hour => ({
+            time: hour.time.split(" ")[1], 
+            temp: hour.temp_c
+        }))
+    }
+}
+
+
